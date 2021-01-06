fb00dc126830e5eebfc12ed4b4b9f3b7f65bc68e
Pierre-Olivier Simonard <pierre.olivier.simonard@gmail.com>
2021-01-06 18:28:16 +0100

    `make all` :  compiles + starts glootalk in python

---
+++ projects/run_glootalk.py
@@ -0,0 +1,7 @@
+import glootalk
+glootalk.start_server(port=4321, log_path=".")
+glootalk.automerge.init(log_path=".")
+
+print("Glootalk : websocket server running. Ready !")
+while True:
+    continue
