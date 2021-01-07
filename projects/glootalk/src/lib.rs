26c62b3c0604e806c7866a0bc236e6463e845ff8
Pierre-Olivier Simonard <pierre.olivier.simonard@gmail.com>
2021-01-07 17:09:44 +0100

    The server creates a doc and sends a valid payload

--- projects/glootalk/src/lib.rs
+++ projects/glootalk/src/lib.rs
@@ -25,6 +25,10 @@ use simplelog::*;
 mod amserver;
 use amserver::init_submodule;
 
+use automerge_backend as amb;
+use automerge_frontend::{Frontend, InvalidChangeRequest, LocalChange, Path, Value};
+use automerge_protocol as amp;
+
 // Start a tungstenite based websocket server
 #[pyfunction]
 fn start_server(port: usize, log_path: &str) {
@@ -47,54 +51,74 @@ fn start_server(port: usize, log_path: &str) {
             spawn(move || {
                 let mut websocket = accept(stream.unwrap()).unwrap();
 
-                // -- The following payload has been extracted raw from the node js version.
-                // It sends to the client an automerge "change" with the initial content text, "Hello"
-                let mut bin_msg = Vec::new();
-                bin_msg.extend(
-                    [
-                        133, 111, 74, 131, 238, 252, 154, 111, 1, 142, 1, 16, 77, 254, 31, 198, 51,
-                        153, 72, 94, 144, 91, 62, 9, 75, 31, 110, 189, 1, 1, 224, 174, 215, 255, 5,
-                        0, 0, 0, 1, 4, 0, 2, 7, 0, 2, 4, 0, 2, 7, 2, 9, 6, 0, 3, 5, 0, 0, 1, 11, 9,
-                        0, 2, 126, 0, 3, 3, 1, 2, 125, 13, 18, 126, 5, 100, 111, 99, 73, 100, 8,
-                        116, 101, 120, 116, 65, 114, 101, 97, 0, 7, 28, 4, 2, 5, 1, 1, 34, 8, 126,
-                        1, 4, 5, 1, 126, 3, 1, 46, 9, 126, 230, 1, 0, 5, 22, 126, 0, 22, 47, 20,
-                        97, 117, 116, 111, 109, 101, 114, 103, 101, 45, 114, 111, 111, 109, 104,
-                        101, 108, 108, 111, 72, 56, 5, 7, 0, 126, 1, 0, 57, 2, 127, 0, 59, 2, 127,
-                        3,
-                    ]
-                    .iter()
-                    .copied(),
+                // --
+                let mut backend = amb::Backend::init();
+                let mut doc = Frontend::new();
+
+                // It's important the docId value passed here ("automerge-room") is equal
+                // to the value set in the client
+                let change = LocalChange::set(
+                    Path::root().key("docId"),
+                    Value::Primitive(amp::ScalarValue::Str("automerge-room".to_string())),
                 );
 
-                websocket
-                    .write_message(tungstenite::Message::Binary(bin_msg))
+                let change_request = doc
+                    .change::<_, InvalidChangeRequest>(Some("set root object".into()), |doc| {
+                        doc.add_change(change)?;
+                        Ok(())
+                    })
                     .unwrap();
 
-                // Instead of this mockup payload, we should send here the initial automerge "change" for
-                // the given document.
-                //-----
+                info!(" change request : {:?}", change_request);
+                let patch = backend
+                    .apply_local_change(change_request.unwrap())
+                    .unwrap()
+                    .0;
+                info!(" patch : {:?}", patch);
+
+                // the textArea key contains the content of the client-side text area
+                let change = LocalChange::set(
+                    Path::root().key("textArea"),
+                    //Value::Primitive(amp::ScalarValue::Str("Hello".to_string())),
+                    Value::Text("Hello".chars().collect()),
+                );
+
+                let change_request = doc
+                    .change::<_, InvalidChangeRequest>(Some("".into()), |doc| {
+                        doc.add_change(change)?;
+                        Ok(())
+                    })
+                    .unwrap();
+
+                info!(" change request : {:?}", change_request);
+
+                let patch = backend
+                    .apply_local_change(change_request.unwrap())
+                    .unwrap()
+                    .0;
+                info!(" patch : {:?}", patch);
+
+                let changes = backend.get_changes(&[]);
+
+                for ch in changes {
+                    info!(" change : {:?}", ch.bytes);
+
+                    let mut bin_msg = Vec::new();
+
+                    bin_msg.extend(ch.bytes.iter().copied());
+                    info!(" bin msg : {:?}", bin_msg);
+
+                    websocket
+                        .write_message(tungstenite::Message::Binary(bin_msg))
+                        .unwrap();
+                }
+
+                info!(" doc state : {:?}", doc.state());
 
                 loop {
                     let msg = websocket.read_message().unwrap();
                     if msg.is_binary() || msg.is_text() {
                         info!("Recieved message: {}", msg);
-
-                    // -- Some examples of messages to write through the websocket ...
-
-                    // -- echo received msg
-                    //  websocket.write_message(msg).unwrap();
-
-                    // -- send a text msg
-                    // websocket
-                    //     .write_message(tungstenite::Message::Text("TEST".to_string()))
-                    //     .unwrap();
-
-                    // -- send a binary msg, represented as a Vec<u8>
-                    //let mut bin_msg = Vec::new();
-                    //bin_msg.extend([3, 1, 4, 1, 5, 9].iter().copied());
-                    // websocket
-                    //     .write_message(tungstenite::Message::Binary(bin_msg))
-                    //     .unwrap();
                     } else {
                         info!("Recieved non bin non txt message: {}", msg);
                     }
